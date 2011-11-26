1fe18104271028b55aa308dbb66e729c2c15a142
akahigeg <akahigeg@gmail.com>
2011-11-26 14:41:39 +0900

    テストをsubjectとletを使ってリファクタリングした

--- lib/mzp.rb
+++ lib/mzp.rb
@@ -6,6 +6,7 @@ class TweetCategorizer
 
     return "!HashTag\t#{tweet}" if tweet =~ /.*#.+$/
     return "Reply\t#{tweet}" if tweet =~ /^@.+? .+$/
+    return "Mention\t#{tweet}" if tweet =~ /^.+ @.+? .+$/
 
     "Normal\t#{tweet}"
   end
--- spec/mzp_spec.rb
+++ spec/mzp_spec.rb
@@ -9,33 +9,47 @@ describe TweetCategorizer do
   before do
     @tc = TweetCategorizer.new
   end
-  
+
   describe "categorize" do
+    subject { @tc.categorize(tweet) }
     context "普通のTweetの場合" do
-      it "「あいうえお」を与えた時にNormal\tあいうえおを返す" do
-        @tc.categorize("Alice\tあいうえお").should ==  "Normal\tあいうえお"
+      context "「あいうえお」を与えた時にNormal\tあいうえおを返す" do
+        let(:tweet) { "Alice\tあいうえお" }
+        it { should == "Normal\tあいうえお" }
       end
 
-      it "「かきくけこ」を与えた時にNormal\tかきくけこを返す" do
-        @tc.categorize("Bob\tかきくけこ").should ==  "Normal\tかきくけこ"
+      context "「かきくけこ」を与えた時にNormal\tかきくけこを返す" do
+        let(:tweet) { "Bob\tかきくけこ" }
+        it { should ==  "Normal\tかきくけこ" }
       end
     end
 
     context "hash tagの場合" do
-      it "「あいうえお #hashtag」を与えたときに「!HashTag\tあいうえお #hashtag」を返す" do
-        @tc.categorize("Alice\tあいうえお #hashtag").should == "!HashTag\tあいうえお #hashtag"
+      context "「あいうえお #hashtag」を与えたときに「!HashTag\tあいうえお #hashtag」を返す" do
+        let(:tweet) { "Alice\tあいうえお #hashtag" }
+        it { should == "!HashTag\tあいうえお #hashtag" }
       end
-      it "「かきくけこ #hashtag」を与えたときに「!HashTag\tかきくけこ #hashtag」を返す" do
-        @tc.categorize("Alice\tかきくけこ #hashtag").should == "!HashTag\tかきくけこ #hashtag"
+      context "「かきくけこ #hashtag」を与えたときに「!HashTag\tかきくけこ #hashtag」を返す" do
+        let(:tweet) { "Alice\tかきくけこ #hashtag" }
+        it { should == "!HashTag\tかきくけこ #hashtag" }
       end
     end
 
     context "replyの場合" do
-      it "「@bob あいうえお」を与えたときに「Reply\tあいうえお」を返す" do
-        @tc.categorize("Alice\t@bob あいうえお").should == "Reply\t@bob あいうえお"
+      context "「@bob あいうえお」を与えたときに「Reply\tあいうえお」を返す" do
+        let(:tweet) { "Alice\t@bob あいうえお" }
+        it { should == "Reply\t@bob あいうえお" }
       end
-      it "「@bob かきくけこ」を与えたときに「Reply\tかきくけこ」を返す" do
-        @tc.categorize("Alice\t@bob かきくけこ").should == "Reply\t@bob かきくけこ"
+      context "「@bob かきくけこ」を与えたときに「Reply\tかきくけこ」を返す" do
+        let(:tweet) { "Alice\t@bob かきくけこ" }
+        it { should == "Reply\t@bob かきくけこ" }
+      end
+    end
+
+    context "mentionの場合" do
+      context "「嗚呼 @bob あいうえお」を与えたときに「Mention\t嗚呼 @bob あいうえお」を返す" do
+        let(:tweet) { "Alice\t嗚呼 @bob あいうえお" }
+        it { should == "Mention\t嗚呼 @bob あいうえお" }
       end
     end
   end
